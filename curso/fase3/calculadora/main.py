4c9966484c49d152766a08bde879de2f688de0c3
PedroH <pedroh422silva@gmail.com>
2024-11-01 12:40:16 -0300

    funcional

--- curso/fase3/calculadora/main.py
+++ curso/fase3/calculadora/main.py
@@ -7,18 +7,24 @@ from PySide6.QtWidgets import QApplication
 from variables import WINDOW_ICON_PATH 
 
 if __name__ == '__main__':
-    # Cria a aplicação
-    app = QApplication(sys.argv)
-    window = MainWindow()
+    try:
+        # Cria a aplicação
+        app = QApplication(sys.argv)
+        window = MainWindow()
 
-    # Define o ícone
-    icon = QIcon(str(WINDOW_ICON_PATH))
-    window.setWindowIcon(icon)
-    app.setWindowIcon(icon)
+        # Define o ícone
+        icon = QIcon(str(WINDOW_ICON_PATH))
+        window.setWindowIcon(icon)
+        app.setWindowIcon(icon)
 
-    # Display
-    display = Display()
-    window.addToVLayout(display)
-    # Executa tudo
-    window.adjustFixedSize()
-    window.show()
+        # Display
+        display = Display()
+        window.addToVLayout(display)
+
+        # Executa tudo
+        window.adjustFixedSize()
+        window.show()
+
+        sys.exit(app.exec())
+    except Exception as e:
+        print(f"An error occurred: {e}")
