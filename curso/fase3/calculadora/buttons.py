d3c7588cfbf5698b186cd2205b6fd25033e1d496
PedroH <pedroh422silva@gmail.com>
2024-11-06 10:14:02 -0300

    n sei mais

--- curso/fase3/calculadora/buttons.py
+++ curso/fase3/calculadora/buttons.py
@@ -1,20 +1,22 @@
-from PySide6.QtWidgets import QPushButton, QGridLayout
-from utils import isEmpty, isNumOrDot
+from display import Display
+from PySide6.QtCore import Slot
+from PySide6.QtWidgets import QGridLayout, QPushButton
+from utils import isEmpty, isNumOrDot, isValidNumber
 from variables import MEDIUM_FONT_SIZE
-
 class Button(QPushButton):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.configStyle()
+
     def configStyle(self):
         font = self.font()
         font.setPixelSize(MEDIUM_FONT_SIZE)
         self.setFont(font)
         self.setMinimumSize(75, 75)
-        self.setProperty('cssClass', 'specialButton')
+        self.setCheckable(True)
         
 class ButtonsGrid(QGridLayout):
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, display: Display, info, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         
         self._gridMask = [
@@ -25,12 +27,34 @@ class ButtonsGrid(QGridLayout):
             ['',  '0', '.', '='],
         ]
         
+        self.display = display
+        self.info = info
         self._makeGrid()
         
     def _makeGrid(self):
         for rowNumber, rowData in enumerate(self._gridMask):
             for colNumber, buttonText in enumerate(rowData):
                 button = Button(buttonText)
+
                 if not isNumOrDot(buttonText) and not isEmpty(buttonText):
                     button.setProperty('cssClass', 'specialButton')
+
                 self.addWidget(button, rowNumber, colNumber)
+                buttonSlot = self._makeButtonDisplaySlot(
+                    self._insertButtonTextToDisplay,
+                    button,
+                )
+                button.clicked.connect(buttonSlot)  # type: ignore
+    def _makeButtonDisplaySlot(self, func, *args, **kwargs):
+        @Slot(bool)
+        def realSlot(_):
+            func(*args, **kwargs)
+        return realSlot
+    def _insertButtonTextToDisplay(self, button):
+        buttonText = button.text()
+        newDisplayValue = self.display.text() + buttonText
+        
+        if not isValidNumber(newDisplayValue):
+            return
+        
+        self.display.insert(buttonText)
