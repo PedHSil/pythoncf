2484c588981f7cfb08cc09e649c4cdda492fc7ba
PedroH <pedroh422silva@gmail.com>
2024-11-06 11:12:01 -0300

    commit

--- curso/fase3/calculadora/main.py
+++ curso/fase3/calculadora/main.py
@@ -1,46 +1,41 @@
 import sys
- 
 from buttons import ButtonsGrid
 from display import Display
 from info import Info
 from main_window import MainWindow
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication
-from styles import setupTheme
 from variables import WINDOW_ICON_PATH
+import qdarkstyle
+
+# Ajuste a função para aplicar o estilo
+def setupTheme(app):
+    app.setStyleSheet(qdarkstyle.load_stylesheet())
 
 if __name__ == '__main__':
-    try:
-        # Cria a aplicação
-        app = QApplication(sys.argv)
-        setupTheme(app)
-        window = MainWindow()
-    
-        # Define o ícone
-        icon = QIcon(str(WINDOW_ICON_PATH))
-        window.setWindowIcon(icon)
-        app.setWindowIcon(icon)
-    
-        # Info
-        info = Info('Sua conta')
-        window.addWidgetToVLayout(info)
-    
-        # Display
-        display = Display()
-        window.addWidgetToVLayout(display)
-        
-         # Grid
-        buttonsGrid = ButtonsGrid(display, info)
-        window.vLayout.addLayout(buttonsGrid)
-    
-        '''# Grid
-        buttonsGrid = ButtonsGrid(display, info, window)
-        window.vLayout.addLayout(buttonsGrid)'''
+    # Cria a aplicação
+    app = QApplication(sys.argv)
+    setupTheme(app)
+    window = MainWindow()
 
-        # Executa tudo
-        window.adjustFixedSize()
-        window.show()
+    # Define o ícone
+    icon = QIcon(str(WINDOW_ICON_PATH))
+    window.setWindowIcon(icon)
+    app.setWindowIcon(icon)
 
-        sys.exit(app.exec())
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    # Info
+    info = Info('Sua conta')
+    window.addWidgetToVLayout(info)
+
+    # Display
+    display = Display()
+    window.addWidgetToVLayout(display)
+
+    # Grid
+    buttonsGrid = ButtonsGrid(display, info, window)
+    window.vLayout.addLayout(buttonsGrid)
+
+    # Executa tudo
+    window.adjustFixedSize()
+    window.show()
+    app.exec()
--- curso/fase3/calculadora/main_window.py
+++ curso/fase3/calculadora/main_window.py
@@ -1,22 +1,26 @@
-from PySide6.QtWidgets import QMainWindow, QVBoxLayout, QWidget
+from PySide6.QtWidgets import QMainWindow, QMessageBox, QVBoxLayout, QWidget
+
 
 class MainWindow(QMainWindow):
     def __init__(self, parent: QWidget | None = None, *args, **kwargs) -> None:
         super().__init__(parent, *args, **kwargs)
-        
-        # conf. basica do layout
+
+        # Configurando o layout básico
         self.cw = QWidget()
         self.vLayout = QVBoxLayout()
         self.cw.setLayout(self.vLayout)
         self.setCentralWidget(self.cw)
-        
-        # titulo
-        self.setWindowTitle('Calculadora(mude se precisar)')
-        
+
+        # Título da janela
+        self.setWindowTitle('Calculadora')
+
     def adjustFixedSize(self):
-        # não mexer no momento
+        # Última coisa a ser feita
         self.adjustSize()
         self.setFixedSize(self.width(), self.height())
-        
+
     def addWidgetToVLayout(self, widget: QWidget):
         self.vLayout.addWidget(widget)
+
+    def makeMsgBox(self):
+        return QMessageBox(self)
