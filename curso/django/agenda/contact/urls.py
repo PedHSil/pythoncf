f3b61e009a3a9bd06f1c98d03fdf6c00f9542c78
PedroH <pedroh422silva@gmail.com>
2024-12-03 11:26:05 -0300

    created

--- curso/django/agenda/contact/urls.py
+++ curso/django/agenda/contact/urls.py
@@ -1,11 +1,16 @@
+# type:ignore
 from django.urls import path
 
 from contact import views
 
 app_name = 'contact'
 
-urlpatterns = [
-    path('<int:contact_id>/', views.contact, name='contact'),  # type:ignore
-    path('search/', views.search, name='search'),  # type:ignore
-    path('', views.index, name='index'),  # type:ignore
+urlpatterns = [ 
+    path('search/', views.search, name='search'),
+    path('', views.index, name='index'),  
+    
+    path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
+    path('contact/create/', views.create, name='create'),
+    path('contact/<int:contact_id>/update/', views.contact, name='contact'),
+    path('contact/<int:contact_id>/delete/', views.contact, name='contact'),
 ]
