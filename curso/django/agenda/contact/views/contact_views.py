9ea26f453a5d98fd68c1506ba5a7f22be9cb380d
PedroH <pedroh422silva@gmail.com>
2024-12-05 14:25:40 -0300

    final

--- curso/django/agenda/contact/views/contact_views.py
+++ curso/django/agenda/contact/views/contact_views.py
@@ -9,9 +9,9 @@ def index(request):
     contacts = Contact.objects \
         .filter(show=True)\
         .order_by('-id')
-        
+
     paginator = Paginator(contacts, 10)
-    page_number = request.GET.get('page')
+    page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
     context = {
@@ -34,16 +34,16 @@ def search(request):
 
     contacts = Contact.objects \
         .filter(show=True)\
-        .filter( 
+        .filter(
             Q(first_name__icontains=search_value) |
             Q(last_name__icontains=search_value) |
             Q(phone__icontains=search_value) |
             Q(email__icontains=search_value)
         )\
         .order_by('-id')
-        
+
     paginator = Paginator(contacts, 10)
-    page_number = request.GET.get('page')
+    page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
     context = {
