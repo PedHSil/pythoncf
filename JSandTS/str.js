dc2b2350e9f83e3ca0d2eed194e6e437a9d87ac2
PedroH <pedroh422silva@gmail.com>
2025-02-13 16:33:02 -0300

    str

---
+++ JSandTS/str.js
@@ -0,0 +1,7 @@
+let umastring = 'Um texto';
+let umastring2 = "Um \"texto\"";
+
+console.log(umastring.charAt(4))
+console.log(umastring2.concat(' em um lindo dia.'));
+console.log(umastring + ' em um lindo dia.');
+console.log('%{umastring} em um lindo dia')
