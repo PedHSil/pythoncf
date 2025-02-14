14a4faca2655a04af01867134db3535a207e196f
PedroH <pedroh422silva@gmail.com>
2025-02-14 10:25:26 -0300

    org

--- JSandTS/str.js
+++ JSandTS/str.js
@@ -4,4 +4,5 @@ let umastring2 = "Um \"texto\"";
 console.log(umastring.charAt(4))
 console.log(umastring2.concat(' em um lindo dia.'));
 console.log(umastring + ' em um lindo dia.');
-console.log('%{umastring} em um lindo dia')
+console.log('%{umastring} em um lindo dia');
+console.log(umastring.slice(-3));
